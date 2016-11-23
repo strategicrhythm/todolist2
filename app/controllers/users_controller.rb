d1cfcbdba140bb9bcbe72e3e4af2b81fe39deaf1
toshikatsu_asai <strategicrhythm@gmail.com>
2016-11-23 09:04:43 +0000

    tab_mypage_work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @todolists = @user.todolists.order(created_at: :desc)
     @todolist = current_user.todolists.build if logged_in?
+    @latest_items = current_user.latest_items.order(created_at: :desc).limit(1) if logged_in?
+    @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc) if logged_in?
   end
   
   def new
