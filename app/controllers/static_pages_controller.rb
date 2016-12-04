1288cd86e44aa0b319fdc45a64783904a7da7be6
toshikatsu_asai <strategicrhythm@gmail.com>
2016-12-04 07:04:14 +0000

    qalist_show_mypage_home_view_work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,7 @@ class StaticPagesController < ApplicationController
     @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc) if logged_in?
     @latest_items = current_user.latest_items.order(created_at: :desc).limit(1) if logged_in?
     @qalist = current_user.qalists.build if logged_in?
+    @latest_qalists = current_user.latest_qalists.order(created_at: :desc).limit(1) if logged_in?
   end
   
 end
